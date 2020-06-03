b7222a3025feb31add884881065a94b146a7f4fb
paulwongx <20178251+paulwongx@users.noreply.github.com>
2020-06-03 15:48:36 -0400

    Updates

---
+++ precache-manifest.bda077edf7f8051077d0451767cad643.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "259722660549ea5b68495bb51fa0e924",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "aa5ad76cc71fb5e70b43",
+    "url": "/pokedex/static/js/2.d15b18a4.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/pokedex/static/js/2.d15b18a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6fb393eeeb30109ca5e",
+    "url": "/pokedex/static/js/main.814ff6c2.chunk.js"
+  },
+  {
+    "revision": "49ddd11826d0d0cf41b6",
+    "url": "/pokedex/static/js/runtime-main.6a17fdf9.js"
+  }
+]);
